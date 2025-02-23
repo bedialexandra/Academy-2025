17441514001bbd6ab9c44997458753918811d15c
bedialexandra <bedialexandra2003@gmail.com>
2025-02-23 15:43:53 +0100

    Add project files.

---
+++ Academy-2025/Controllers/CourseController.cs
@@ -0,0 +1,80 @@
+﻿using Academy_2025.Data;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Academy_2025.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        public static List<Course>? Courses = new List<Course>();
+
+        // GET: api/<CourseController>
+        [HttpGet]
+        public IEnumerable<Course> Get()
+        {
+            return Courses;
+        }
+
+        // GET api/<CourseController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Course> Get(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    return Ok(course);
+                }
+            }
+
+            return NotFound();
+        }
+
+        // POST api/<CourseController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Course data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Courses.Add(data);
+            return NoContent();
+        }
+
+        // PUT api/<CourseController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Course data)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    course.Name = data.Name;
+                    course.Description = data.Description;
+                    return NoContent();
+                }
+            }
+            return NotFound();
+        }
+
+        // DELETE api/<CourseController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    Courses.Remove(course);
+                    return NoContent();
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Academy-2025/Data/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace Academy_2025.Data
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
