93c1f1d8c413426fe04e29142c7e0a4f7d1dae69
bedialexandra <bedialexandra2003@gmail.com>
2025-03-16 20:20:18 +0100

    add author

--- Academy-2025/Data/Course.cs
+++ Academy-2025/Data/Course.cs
@@ -6,6 +6,8 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
 
+        public string? Author { get; set; }
+
         public ICollection<User> Users { get; set; } = [];
 
     }
---
+++ Academy-2025/Migrations/20250316192003_AddRoleAndAuthor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Academy_2025.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoleAndAuthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Courses",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Courses");
+        }
+    }
+}
